07178b42538beaf72d0683c48518541026a88669
WrathfulSpatula <stranoj@gmail.com>
2019-12-07 21:51:25 -0500

    Typo

--- pyquil_sycamore_approximation.py
+++ pyquil_sycamore_approximation.py
@@ -21,7 +21,7 @@ def sqrty(t):
 
 # pyquil/QVM has a universal gate set. It is therefore possible decompose "sqrth" into other gates in the API.
 # However, this is not necessarily practical or representative. For an "apples-to-apples" comparison with
-# the other simulators, making reasonable allowance for easily-implemetned extensions to the simulator API,
+# the other simulators, making reasonable allowance for easily-implemented extensions to the simulator API,
 # replacing "sqrth" with "H" is probably a fairer test.
 
 # Implementation of Sycamore circuit
