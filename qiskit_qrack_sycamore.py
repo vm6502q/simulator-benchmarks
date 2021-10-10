3ce4f9c0cec4e62526b589cb3c8d7e6ebd3045b5
Daniel Strano <stranoj@gmail.com>
2021-10-10 16:35:36 -0400

    Transpile

--- qiskit_qrack_sycamore.py
+++ qiskit_qrack_sycamore.py
@@ -9,6 +9,7 @@ import math
 
 from qiskit import QuantumCircuit
 from qiskit import execute
+from qiskit.compiler.transpiler import transpile
 from qiskit.providers.qrack import QasmSimulator
 
 def sqrtx(circ, t):
@@ -83,6 +84,7 @@ def bench(num_qubits, depth):
     circ = QuantumCircuit(num_qubits, num_qubits)
     sycamore_circuit(num_qubits, depth, circ)
     start = time.time()
+    circ = transpile(circ, backend=sim_backend, optimization_level=3)
     job = execute([circ], sim_backend, timeout=600)
     result = job.result()
     return time.time() - start
