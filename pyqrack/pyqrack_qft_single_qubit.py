e6503a0f2208c6ff9154defaea177d86d3a7be6f
Daniel Strano <stranoj@gmail.com>
2022-07-14 08:47:54 -0400

    PyQrack and QCGPU single-qubit arbitrary width QFT

---
+++ pyqrack/pyqrack_qft_single_qubit.py
@@ -0,0 +1,92 @@
+#Adapted from https://github.com/libtangle/qcgpu/blob/master/benchmark/benchmark.py by Adam Kelly
+
+import click
+import time
+import random
+import csv
+import os.path
+import math
+
+from pyqrack import QrackSimulator, Pauli
+
+# NOTE - |0> or any permutation basis eigenstate QFT is "trivial" for Qrack, and not a fairly
+#  representative test of general QFT performance, in realistic use cases. Hence, this script
+#  applies a random unitary gate to every qubit in the width before carrying out the QFT.
+#  As a result, every other simulator gets a significant handicap, but a representative one.
+
+def phase_root_n(sim, n, q):
+    sim.mtrx([1, 0, 0, -1**(1.0 / (1<<(n - 1)))], q)
+
+def bench(sim, num_qubits):
+    sim.reset_all()
+    m_results = []
+    start = time.time()
+    for _ in range(num_qubits):
+        sim.u(0, random.uniform(0, 2 * math.pi), random.uniform(0, 2 * math.pi), random.uniform(0, 2 * math.pi))
+
+        # We use the single control qubit "trick" referenced in Beauregard:
+        for j in range(len(m_results)):
+            if m_results[j]:
+                phase_root_n(sim, j + 2, 0)
+
+        m_results.append(sim.m(0))
+        if m_results[-1]:
+            sim.x(0)
+
+    return time.time() - start
+
+# Reporting
+def create_csv(filename):
+    file_exists = os.path.isfile(filename)
+    csvfile = open(filename, 'a')
+   
+    headers = ['name', 'num_qubits', 'time']
+    writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
+
+    if not file_exists:
+        writer.writeheader()  # file doesn't exist yet, write a header
+
+    return writer
+
+def write_csv(writer, data):
+    writer.writerow(data)
+
+
+
+@click.command()
+@click.option('--samples', default=100, help='Number of samples to take for each qubit.')
+@click.option('--qubits', default=30, help='How many qubits you want to test for')
+@click.option('--out', default='benchmark_data.csv', help='Where to store the CSV output of each test')
+@click.option('--single', default=False, help='Only run the benchmark for a single amount of qubits, and print an analysis')
+def benchmark(samples, qubits, out, single):
+    if single:
+        low = qubits - 1
+    else:
+        low = 3
+    high = qubits
+
+    functions = bench,
+    writer = create_csv(out)
+
+    for n in range(low, high):
+        sim = QrackSimulator(1)
+
+        # Progress counter
+        progress = (n - low) / (high - low)
+        print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(progress * 50), progress*100), end="", flush=True)
+
+        # Run the benchmarks
+        for i in range(samples):
+            try:
+                t = bench(sim, n)
+                write_csv(writer, {'name': 'pyqrack_qft', 'num_qubits': n+1, 'time': t})
+            except:
+                del sim
+                write_csv(writer, {'name': 'pyqrack_qft', 'num_qubits': n+1, 'time': -999})
+                sim = QrackSimulator(n + 1)
+
+        # Call old simulator width destructor BEFORE initializing new width
+        del sim
+
+if __name__ == '__main__':
+    benchmark()
---
+++ qcgpu/qcgpu_qft_single_qubit.py
@@ -0,0 +1,96 @@
+#Reduced directly from https://github.com/libtangle/qcgpu/blob/master/benchmark/benchmark.py by Adam Kelly (with thanks)
+
+import click
+import time
+import random
+import statistics
+import csv
+import os.path
+import math
+import numpy as np
+
+import qcgpu
+
+def phase_root_n(sim, n, q):
+    phase_root_n_matrix = np.array([
+        [1, 0],
+        [0, np.exp(1j * np.pi / 4)]
+    ])
+    phase_root_n_gate = qcgpu.Gate(phase_root_n_matrix)
+    sim.apply_gate(phase_root_n_gate, q)
+
+def u(sim, th, ph, lm, q):
+    cos0 = math.cos(th / 2);
+    sin0 = math.sin(th / 2);
+    u_matrix = np.array([
+        [cos0, sin0 * (-math.cos(lm) - math.sin(lm) * 1j)],
+        [sin0 * (math.cos(ph) + math.sin(ph) * 1j), cos0 * (math.cos(ph + lm) + math.sin(ph + lm) * 1j)]
+    ])
+    u_gate = qcgpu.Gate(u_matrix)
+    sim.apply_gate(u_gate, q)
+
+def bench(num_qubits):
+    state = qcgpu.State(1)
+    m_results = []
+    start = time.time()    
+    for _ in range(num_qubits):
+        u(state, random.uniform(0, 2 * math.pi), random.uniform(0, 2 * math.pi), random.uniform(0, 2 * math.pi), 0)
+
+        # We use the single control qubit "trick" referenced in Beauregard:
+        for j in range(len(m_results)):
+            if m_results[j]:
+                phase_root_n(state, j + 2, 0)
+
+        m_results.append(state.measure())
+        if m_results[-1]:
+            state.x(0)
+
+    return time.time() - start
+    
+
+# Reporting
+def create_csv(filename):
+    file_exists = os.path.isfile(filename)
+    csvfile = open(filename, 'a')
+   
+    headers = ['name', 'num_qubits', 'time']
+    writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
+
+    if not file_exists:
+        writer.writeheader()  # file doesn't exist yet, write a header
+
+    return writer
+
+def write_csv(writer, data):
+    writer.writerow(data)
+
+
+
+@click.command()
+@click.option('--samples', default=100, help='Number of samples to take for each qubit.')
+@click.option('--qubits', default=28, help='How many qubits you want to test for')
+@click.option('--out', default='benchmark_data.csv', help='Where to store the CSV output of each test')
+@click.option('--single', default=False, help='Only run the benchmark for a single amount of qubits, and print an analysis')
+def benchmark(samples, qubits, out, single):
+    if single:
+        low = qubits - 1
+    else:
+        low = 3
+    high = qubits
+
+    functions = bench,
+    writer = create_csv(out)
+
+    for n in range(low, high):
+        # Progress counter
+        progress = (n - low) / (high - low)
+        print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(progress * 50), progress*100), end="", flush=True)
+
+        # Run the benchmarks
+        for i in range(samples):
+            func = random.choice(functions)
+            t = func(n + 1)
+            write_csv(writer, {'name': 'qcgpu_qft', 'num_qubits': n+1, 'time': t})
+
+if __name__ == '__main__':
+    benchmark()
