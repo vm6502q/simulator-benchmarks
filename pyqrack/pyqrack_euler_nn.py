fe0c699a83ca3b30f39f2fe742428616000447aa
Daniel Strano <stranoj@gmail.com>
2022-07-31 12:32:29 -0400

    Restore try/catch

--- pyqrack/pyqrack_euler_nn.py
+++ pyqrack/pyqrack_euler_nn.py
@@ -137,12 +137,12 @@ def benchmark(samples, qubits, depth, out, single):
 
             # Run the benchmarks
             for i in range(samples):
-                #try:
+                try:
                     t = bench(sim, d + 1)
                     write_csv(writer, {'name': 'pyqrack_euler_nn', 'num_qubits': n+1, 'depth': d+1, 'time': t})
-                #except:
-                #    del sim
-                #    write_csv(writer, {'name': 'pyqrack_euler_nn', 'num_qubits': n+1, 'depth': d+1, 'time': -999})
+                except:
+                    del sim
+                    write_csv(writer, {'name': 'pyqrack_euler_nn', 'num_qubits': n+1, 'depth': d+1, 'time': -999})
                     sim = QrackSimulator(n + 1)
 
         del sim
